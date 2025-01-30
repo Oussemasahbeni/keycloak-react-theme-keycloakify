fc7b7bbcc055c8387e6c6d5a08996bf27633eb84
Oussema Sahbeni <oussemasahbeni300@gmail.com>
2025-01-30 09:53:22 +0100

    chore: remove jsx-email configuration and related dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -3,22 +3,13 @@ import { keycloakify } from "keycloakify/vite-plugin";
 import path from "path";
 import { defineConfig } from "vite";
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         react(),
         keycloakify({
-            accountThemeImplementation: "none"
-            // postBuild: async buildContext => {
-            //     await buildEmailTheme({
-            //         templatesSrcDirPath: import.meta.dirname + "/src/emails/templates",
-            //         themeNames: buildContext.themeNames,
-            //         keycloakifyBuildDirPath: buildContext.keycloakifyBuildDirPath,
-            //         locales: ["en", "fr"],
-            //         cwd: import.meta.dirname,
-            //         esbuild: {} // optional esbuild options
-            //     });
-            // }
+            accountThemeImplementation: "none",
         })
     ],
     resolve: {
