8ccc8489ef9799fec52f33ff4b603eb47819f733
oussema sahbeni <oussemasahbeni300@gmail.com>
2025-12-08 16:44:18 +0100

    refactor: update CI workflow to build Storybook and change artifact path

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,15 +2,11 @@ import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
     stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-    addons: [
-        "@storybook/addon-onboarding",
-        "@storybook/addon-essentials",
-        "@chromatic-com/storybook",
-        "@storybook/addon-interactions"
-    ],
+    addons: [],
     framework: {
         name: "@storybook/react-vite",
         options: {}
-    }
+    },
+    staticDirs: ["../public"]
 };
 export default config;
